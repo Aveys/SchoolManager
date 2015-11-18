87fa9081c8de91498ea1aad146c2655ef01262dc
lblain69 <lblain69@gmail.com>
2015-11-18 16:57:53 +0100

    EcoleSubmission Added

---
+++ SchoolManager-EJB/ejbModule/model/EcoleSubmission.java
@@ -0,0 +1,20 @@
+package model;
+
+import entities.Ecole;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class EcoleSubmission extends Ecole{
+	
+	/**
+	 * Generated UID
+	 */
+	private static final long serialVersionUID = 7862494688713938372L;
+
+	public EcoleSubmission(){
+	}
+	
+}

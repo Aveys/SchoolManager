903f1e542f49982914e5b819e59b030827762c55
lblain69 <lblain69@gmail.com>
2015-11-18 16:24:52 +0100

    LoginBean added

---
+++ SchoolManager-EJB/ejbModule/services/UserServices.java
@@ -0,0 +1,12 @@
+package services;
+
+import java.util.List;
+
+import entities.User;
+
+public interface UserServices {
+	
+	public User getUser(String mail, String motDePasse);
+	public void addUser(User user);
+	public List<User> getListUser();
+}

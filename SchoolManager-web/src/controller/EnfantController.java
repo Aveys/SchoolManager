d355c760a8bc4a3b865749f445c817da9fd997ec
arca223 <arcanis223@gmail.com>
2015-11-19 17:05:33 +0100

    enfant controller

--- SchoolManager-web/src/controller/EnfantController.java
+++ SchoolManager-web/src/controller/EnfantController.java
@@ -3,7 +3,19 @@ package controller;
 import java.util.List;
 
 import entities.Enfant;
+import services.impl.ClasseServicesImpl;
+import services.impl.EnfantServicesImpl;
 
 public class EnfantController {
+	
+	public List<Enfant> getEnfants() {
+		EnfantServicesImpl enfServ = new EnfantServicesImpl();
+		return enfServ.getListEnfant();
+	}
+	
+	public String getClasse(int id) {
+		ClasseServicesImpl claServ = new ClasseServicesImpl();
+		return Integer.toString(claServ.getClasse(id).getNumClasse());
+	}
 
 }
